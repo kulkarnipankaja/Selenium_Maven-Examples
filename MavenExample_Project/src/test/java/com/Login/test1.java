a713ed564ba01c943ce5442c17414188cf4553ac
pankaja <pankaja@DESKTOP-986UGOM>
2017-04-24 11:15:23 -0700

    commiting Maven Exmaple Files

---
+++ MavenExample_Project/src/test/java/com/Login/test1.java
@@ -0,0 +1,14 @@
+package com.Login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) throws Exception  {
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+
+	}
+
+}

a911f48d120b96ea85ab12dda636a4609aacc0db
pankaja <pankaja@DESKTOP-986UGOM>
2017-04-24 11:35:31 -0700

    updated  code with Firefox Driver

--- MavenExample_Project/src/test/java/com/Login/test1.java
+++ MavenExample_Project/src/test/java/com/Login/test1.java
@@ -1,13 +1,22 @@
 package com.Login;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class test1 {
 
 	public static void main(String[] args) throws Exception  {
 		
-		ChromeDriver driver=new ChromeDriver();
-		driver.get("https://www.facebook.com/");
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium\\geckodriver.exe");
+		driver =new FirefoxDriver();
+		driver.get("http://demoaut.com");
+		Thread.sleep(3000);
+		//System.out.println("Inside Before method");
+		
+//		ChromeDriver driver=new ChromeDriver();
+//		driver.get("https://www.facebook.com/");
 
 	}
 
